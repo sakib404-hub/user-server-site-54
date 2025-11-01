be680281d620c0f7e60d48e76114280478300381
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-11-02 00:54:28 +0600

    connected the mongoDB server SuccessFUlly

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5010;
 
 app.use(cors());
 app.use(express.json());
+
+// userName  : userManagemnet
+// password: T8YDF2s316qmXSVH;
+const uri =
+  "mongodb+srv://userManagemnet:T8YDF2s316qmXSVH@crud-operation.iftbw43.mongodb.net/?appName=CRUD-operation";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    // trying to connect with the mongoDB
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged the Deployment!Successfully!");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hellow From the server Api!");
